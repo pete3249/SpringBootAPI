aaf98e8f4a22951d4be2d86b2dd2dc7eb02694eb
pete3249 <katelynrpeterson16@gmail.com>
2021-06-13 16:59:58 -0500

    Add logic for adding new message

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -24,6 +25,12 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
         System.out.println(student);
     }
 }
