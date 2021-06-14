118d12a8b9504606dac16209064f9e702ff23172
pete3249 <katelynrpeterson16@gmail.com>
2021-06-14 10:30:35 -0500

    Implement delete functionality

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -24,5 +25,10 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -32,5 +33,14 @@ public class StudentService {
         }
         studentRepository.save(student);
         System.out.println(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+       boolean exists = studentRepository.existsById(studentId);
+       if (!exists) {
+           throw new IllegalStateException(
+                   "student with id " + studentId + " does not exist");
+       }
+       studentRepository.deleteById(studentId);
     }
 }
